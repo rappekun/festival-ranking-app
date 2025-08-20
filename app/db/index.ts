8068a9b57a9ea2606f2f41ec7e4167421938be26
rappekun <rappekun777@gmail.com>
2025-08-20 12:24:31 +0000

    fix: reorganize dotenv configuration and clean up CrownIcon component syntax

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,6 +1,10 @@
 import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
+
 import * as schema from "./schema/";
+
+dotenv.config();
 
 const sql = neon(process.env.DATABASE_URL as string);
 const db = drizzle({ client: sql, schema: schema });
--- app/routes/_auth/_components/crown.tsx
+++ app/routes/_auth/_components/crown.tsx
@@ -1,6 +1,6 @@
 import type { FC, SVGProps } from "react";
 
-const CrownIcon: FC<SVGProps<SVGSVGElement>> = (props) => (
+const CrownIcon: FC<SVGProps<SVGSVGElement>> = props => (
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		width="200"
